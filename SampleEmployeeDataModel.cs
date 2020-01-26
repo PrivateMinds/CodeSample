31b2b4cff19c05260f3303dc7b393d6663bfdb7a
PrivateMinds <56062419+PrivateMinds@users.noreply.github.com>
2020-01-25 20:07:56 -0800

    Add files via upload

---
+++ SampleEmployeeDataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDataFactory.DataModels
+{
+    public class SampleEmployeeDataModel
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string SSN { get; set; }
+    }
+
+}
