d28201d8dc5fc7d94cffe56904a8e856927b7e9b
PrivateMinds <56062419+PrivateMinds@users.noreply.github.com>
2020-01-25 20:16:00 -0800

    Add files via upload

---
+++ GetData.cs
@@ -0,0 +1,129 @@
+﻿using SampleDataFactory.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Date: 01-22-2020
+/// This is an example of an ADO.NET middle-tier get data class.   
+/// This class contains method (s) that perform the following actions:
+///     1- validates user access.
+///     2- Retrieves  data from the database.
+/// </summary>
+/// 
+namespace SampleDataFactory
+{
+    public class GetData
+    {
+        
+        /// <summary>
+        ///  ===== EXAMPLE METHOD 1 ============
+        ///  This  method is to be used to validate user access. 
+        /// </summary>
+        /// <param name="CurrentUser"></param>
+        /// <returns>
+        ///  Returns true for 'granted access' and false for 'denied access'</returns>
+        public string CheckuserSecurityAccess(string CurrentUser)
+        {
+            string ReturnedValue = "false";
+
+            SqlConnection SQLConn = new SqlConnection();
+            SqlCommand SQLComm = new SqlCommand();
+            SqlDataReader SQLRec;
+
+            //connection string to the database
+            SQLConn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["EmployeeDataBaseConnectionString"].ConnectionString;
+
+            //open connection
+            SQLConn.Open();
+
+            //stored procedure name
+            string SQL = "ssp_tblOperator_GetUserAccess";
+            SQLComm = new SqlCommand(SQL, SQLConn);
+            SQLComm.CommandType = CommandType.StoredProcedure;
+
+           //parameter passed
+            SQLComm.Parameters.AddWithValue("@CurrentUser", CurrentUser);
+
+            SQLRec = SQLComm.ExecuteReader();
+
+            try
+            {
+                if (SQLRec.Read())
+                {
+                    //returned results
+                    ReturnedValue = SQLRec.GetString(SQLRec.GetOrdinal("UserHasAccess"));
+                }
+            }
+            catch (Exception e)
+            {
+
+                ReturnedValue = e.Message;
+
+            }
+
+            //close connection
+            SQLRec.Close();
+            SQLConn.Close();
+            
+            return ReturnedValue;
+        }
+
+        /// <summary>
+        /// ===== EXAMPLE METHOD 2 ============
+        /// </summary>
+        /// This method is to be used to return a list of employee information.
+         /// <returns> 
+         ///  A list of employee information
+         /// </returns>
+        public List<SampleEmployeeDataModel> GetCodeSampleList()
+        {
+            
+            List<SampleEmployeeDataModel> SampleDataModel = new List<SampleEmployeeDataModel>();
+
+            SqlConnection SQLconn = new SqlConnection();
+            SqlCommand SQLcomm = new SqlCommand();
+            SqlDataReader SQLRec;
+
+            //connection string to the database
+            SQLconn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["EmployeeDataBaseConnectionString"].ConnectionString;
+
+            //open connection
+            SQLconn.Open();
+
+            //stored procedure name
+            string SQL = "ssp_tblEmployee_GetEmployeeInformationList";
+
+            SQLcomm = new SqlCommand(SQL, SQLconn);
+            SQLcomm.CommandType = CommandType.StoredProcedure;
+            
+            SQLRec = SQLcomm.ExecuteReader();
+
+            //read data into the model
+            while (SQLRec.Read())
+            {
+                SampleDataModel.Add(new SampleEmployeeDataModel
+                {
+                    ID = SQLRec.GetInt32(SQLRec.GetOrdinal("ID")),
+                    FirstName = SQLRec.GetString(SQLRec.GetOrdinal("FirstName")),
+                    LastName = SQLRec.GetString(SQLRec.GetOrdinal("LastName")),
+                    DOB = SQLRec.GetDateTime(SQLRec.GetOrdinal("DOB")),
+                    SSN = SQLRec.GetString(SQLRec.GetOrdinal("SSN")),
+                 
+                 
+                });
+            }
+
+            //close connection
+            SQLRec.Close();
+            SQLconn.Close();
+
+            return SampleDataModel;
+        }
+
+    }
+}
---
+++ SaveData.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SampleDataFactory.DataModels;
+
+namespace SampleDataFactory
+{
+    /// <summary>
+    /// Date: 01-22-2020
+    /// This is an example of an ADO. NET middle-tier save data class.   
+    /// This class contains method (s) that perform the following actions:
+    ///     1- Save and update data.
+    /// </summary>
+    /// 
+    public class SaveData
+    {
+        /// <summary>
+        /// ===== Example Method 1 ======
+        /// This method is to be used to save new or update existing employee  information.
+        /// </summary>
+        /// <param name="SampleEmployeeDataModel"></param>
+        /// <returns>
+        /// returns an actual ID for 'success' and 0 for 'failure'
+        /// </returns>
+        public string SaveEmployeeInformation(SampleEmployeeDataModel SampleEmployeeDataModel)
+        {
+            //variable that holds the returned id.
+            string NewID = "0";
+
+            //connection string
+            SqlConnection SQLConn = new SqlConnection();
+            SQLConn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["EmployeeDataBaseConnectionString"].ConnectionString;
+
+            //open connection
+            SQLConn.Open();
+
+            //try catch block
+            try
+            {
+                //stored procedure name
+                SqlCommand SQLComm = new SqlCommand("[ssp_tblEmployee_SaveNewOrUpdateEmployeeInformation]", SQLConn);
+                SQLComm.CommandType = CommandType.StoredProcedure;
+
+                //passed parameters 
+                SQLComm.Parameters.AddWithValue("@ID", SampleEmployeeDataModel.ID);
+                SQLComm.Parameters.AddWithValue("@FirstName", SampleEmployeeDataModel.FirstName);
+                SQLComm.Parameters.AddWithValue("@LastName", SampleEmployeeDataModel.LastName);
+                SQLComm.Parameters.AddWithValue("@DOB", SampleEmployeeDataModel.DOB);
+                SQLComm.Parameters.AddWithValue("@SSN", SampleEmployeeDataModel.SSN);
+
+                //Returned ID as an output parameter.
+                SQLComm.Parameters.Add("@new_ID", SqlDbType.Int).Direction = ParameterDirection.Output;
+                
+                SQLComm.ExecuteNonQuery();
+
+                //set your variable to the returned value
+                NewID = SQLComm.Parameters["@new_ID"].Value.ToString();
+            }
+
+            catch (Exception e)
+            {
+                string msg = e.Message;
+                NewID = NewID + " " + msg;
+            }
+            finally
+            {
+                //close connection
+                SQLConn.Close();
+            }
+
+            return NewID;
+        }
+    }
+}
