589d8c00606950a2768186899f36f868b9be36cd
PrivateMinds <56062419+PrivateMinds@users.noreply.github.com>
2020-01-25 20:10:52 -0800

    Add files via upload

---
+++ CodeSampleEmployeeModel.cs
@@ -0,0 +1,21 @@
+﻿using SampleDataFactory.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeSample.Models
+{
+    public class CodeSampleEmployeeModel
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string SSN { get; set; }
+
+        //This list will be used to render data in code sample view
+        public List<SampleEmployeeDataModel> GetCodeSampleList { get; set; }
+
+    }
+}
